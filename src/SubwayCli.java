9e71ed95595b744201fb1ac9fe625e022712bf06
ella <lydbsdud@gmail.com>
2025-09-14 15:39:13 +0900

    feat: 메뉴 선택 기능 추가 - Menu 클래스로 분리 메뉴 출력 분리 - Choose 클래스로 입력 기능 구현

--- src/SubwayCli.java
+++ src/SubwayCli.java
@@ -1,41 +1,18 @@
-import domain.Product;
-import domain.Salad;
-import domain.Sandwich;
-
-import java.util.ArrayList;
-import java.util.List;
+import domain.Choose;
+import domain.Menu;
 
 public class SubwayCli {
-    private final List<Product> products = new ArrayList<>();
+    Menu menu = new Menu();
+    Choose choose = new Choose();
 
-    public SubwayCli() {
-        products.add(new Sandwich("치킨 데리야끼", 7000, 12900));
-        products.add(new Sandwich("스파이시 이탈리안", 6900, 12700));
-        products.add(new Sandwich("치킨 베이컨 아보카도", 7900, 14700));
-        products.add(new Sandwich("비엘티", 6600, 12200));
-        products.add(new Sandwich("폴드포크 바베큐", 7200, 13300));
-        products.add(new Sandwich("쉬림프", 7600, 14100));
-        products.add(new Salad("베지 샐러드", 6700));
-        products.add(new Salad("로스트 치킨 샐러드", 9100));
-        products.add(new Salad("로티세리 바베큐 치킨 샐러드", 9100));
-    }
-
-    public void displayMenu() {
-        System.out.println("\n--------------------메뉴--------------------");
-        for(int i=0; i<products.size(); i++) {
-            Product product = products.get(i);
-            System.out.print((i+1)+"."+ product.getName()+" ");
-            if (product instanceof Sandwich sandwich) {
-                System.out.printf("(15cm: %d원 | 30cm: %d원)\n", sandwich.getPrice15cm(), sandwich.getPrice30cm());
-            } else if (product instanceof Salad salad) {
-                System.out.printf("(%d원)\n", salad.getPrice());
-            }
-        }
-        System.out.println("-------------------------------------------");
+    public void chooseMenu() {
+        menu.displayMenu();
+        int menuCount = menu.getMenuCount();
+        choose.oneChoose("메뉴를 선택해주세요.  ",1, menuCount);
     }
     public static void main(String[] args) {
         SubwayCli subwayCli = new SubwayCli();
         System.out.println("어서오세요 서브웨이 입니다!!");
-        subwayCli.displayMenu();
+        subwayCli.chooseMenu();
     }
 }
